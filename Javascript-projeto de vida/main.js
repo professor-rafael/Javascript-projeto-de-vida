e356cf6be4d9c966e39bbdb2591715d07b0de9b4
Professor-Rafael <101332916+professor-rafael@users.noreply.github.com>
2024-02-26 14:56:25 -0300

    quase lá

---
+++ Javascript-projeto de vida/main.js
@@ -0,0 +1,12 @@
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+    }
+}
